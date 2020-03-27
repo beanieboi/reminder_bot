ddcf0a8ad9e574cab09ffae67676e7d5e4a61c2c
ben <beanie@benle.de>
2020-03-27 22:52:55 +0100

    Something something

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,28 @@
 import { resolve } from "path"
 import { config } from "dotenv"
+const { Client } = require('pg')
 
 import Telegraf from "telegraf"
 
-config({ path: resolve(__dirname, "../.env") })
+const client = new Client()
 
-const bot = new Telegraf(process.env.BOT_TOKEN)
-bot.start((ctx) => ctx.reply('Welcome'))
-bot.hears('hi', (ctx) => ctx.reply('Hey there'))
-bot.launch()
+
+async function start() {
+  await client.connect()
+
+  config({ path: resolve(__dirname, "../.env") })
+
+  const bot = new Telegraf(process.env.BOT_TOKEN)
+
+  const res = await client.query('SELECT NOW() as message')
+  console.log(res.rows[0].message)
+  await client.end()
+
+
+  bot.start((ctx) => ctx.reply('Welcome'))
+  bot.hears('hi', (ctx) => ctx.reply('Hey there'))
+  bot.launch()
+
+}
+
+start()
