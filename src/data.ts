4573664e43f4763a712abbe6a6f28b0a2903ff35
Kristin Fritsch <ich@kristin-fritsch.de>
2020-03-28 15:13:18 +0100

    Add data

--- src/data.ts
+++ src/data.ts
@@ -1,5 +1,14 @@
+const channel_id: number = 1234
+
+enum Status {
+  SNOOZED = 'snoozed',
+  COMPLETED = 'completed',
+  CANCELED = 'canceled'
+}
+
 interface Task {
   id: number,
+  channel_id: number,
   text: string,
   start_time: number,
   interval_hours: number,
@@ -7,9 +16,16 @@ interface Task {
   snooze_default_minutes: number
 }
 
-const tasks: Task[] = [
+interface Reminder {
+  id: number,
+  task_id: number,
+  status: Status | null
+}
+
+const tasksData: Task[] = [
   {
     id: 1,
+    channel_id: channel_id,
     text: "Time to take your meds",
     start_time: Date.now(),
     interval_hours: 12,
@@ -18,6 +34,7 @@ const tasks: Task[] = [
   },
   {
     id: 2,
+    channel_id: channel_id,
     text: "Time to take water the plants",
     start_time: Date.now(),
     interval_hours: 24,
@@ -25,3 +42,18 @@ const tasks: Task[] = [
     snooze_default_minutes: 12
   }
 ]
+
+const reminderData: Reminder[] = [
+  {
+    id: 1,
+    task_id: tasksData[0].id,
+    status: null
+  },
+  {
+    id: 2,
+    task_id: tasksData[1].id,
+    status: null
+  }
+]
+
+export {Status, tasksData, reminderData}
